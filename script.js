fed4d28e22f6ef4e2887bf6389db20987a522169
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-28 20:48:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS code here.
+function checkSlide(e) {
+	const images = document.querySelectorAll(".slide-in");
+	
+	images.forEach((image) => {
+	const slideInAt =
+	window.scrollY + window.innerHeight - image.height / 2;
+	const imageBottom = image.offsetTop + image.height;
+	const isHalfShown = slideInAt > image.offsetTop;
+	const isNotScrolledPast = window.scrollY < imageBottom;
+	if (isHalfShown && isNotScrolledPast) {
+	image.classList.add("active");
+	} else {
+	image.classList.remove("active");
+	}
+	});
+	}
